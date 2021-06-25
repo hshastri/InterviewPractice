2d5108bde2cc65e4f143fdba4f412ab6843802fe
Harsh Shastri <harshshastri03@gmail.com>
2021-06-25 17:42:10 -0400

    Optimized (II)

--- Practice/RepeatedString.py
+++ Practice/RepeatedString.py
@@ -2,6 +2,20 @@ def repeatedString(s, n):
     L = len(s)
     return s.count('a') * (n // L) + s[:n % L].count('a')
 
+"""
+def repeatedString(s, n):
+    # Write your code here
+    frequency = getFrequencyOfA(s)
+    return ((n // len(s)) * frequency) + getFrequencyOfA(s[0 : (n % len(s))])
+    
+def getFrequencyOfA(s):
+    count = 0
+    for char in s:
+        if char == 'a':
+            count+= 1    
+    return count
+"""
+
 """def repeatedString(s, n):
     tempStr = stringBuilder(s, n)
     count = 0
