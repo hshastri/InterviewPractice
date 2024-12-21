01778c755b562f1fa957c4a82c08ea15bb380678
Harsh Shastri <hshastri@pendantautomation.com>
2024-12-21 00:19:01 -0500

    Helpful print statements

--- Algorithms/QuickSort.py
+++ Algorithms/QuickSort.py
@@ -1,40 +1,43 @@
 def QuickSort(arr):
-    return QuickSortHelper(arr, 0, len(arr) - 1)
-
+	return QuickSortHelper(arr, 0, len(arr) - 1)
 
 def QuickSortHelper(arr, left, right):
-    if left >= right:
-        return
-    pivot = (left + right) // 2
-    index = partition(arr, left, right, pivot)
-    QuickSortHelper(arr, left, index - 1)
-    # print(arr[left:index])
-    QuickSortHelper(arr, index, right)
-    # print(arr[index: right + 1])
-
-    """if left < right:
-        pivot = (left + right) // 2
-        index = partition(arr, left, right, pivot)
-        QuickSortHelper(arr, left, index - 1)
-        # print(arr[left:index])
-        QuickSortHelper(arr, index, right)
-        # print(arr[index: right + 1])"""
-
+	if (left >= right):
+		print(f" BASE CASE: left: {left}, right: {right}")
+		return
+	pivot : int = (left + right ) // 2
+	print(f"pivot: {pivot}")
+	index : int = partition(arr, left, right, pivot)
+	print(f"index: {arr[index]}")
+	print("Recursive call stack ")
+	print(f"left: {arr[left: index]}")
+	print(f"right: {arr[index: right + 1]}")
+	print()
+	QuickSortHelper(arr, left, index - 1)
+	QuickSortHelper(arr, index, right)
 
 def partition(arr, left, right, pivot):
-    while left <= right:
-        while arr[left] < arr[pivot]:
-            left += 1
-        while arr[right] > arr[pivot]:
-            right -= 1
-        if left <= right:
-            swap(arr, left, right)
-            left += 1
-            right -= 1
-    return left
+	while (left <= right):
+		while (arr[left] < arr[pivot]):
+			left += 1
+		while (arr[right] > arr[pivot]):
+			right -= 1
+		if (left <= right):
+			swap(arr, left, right)
+			left += 1
+			right -= 1
+	return left
 
+def swap(arr, i : int, j: int):
+	temp = arr[i]
+	arr[i] = arr[j]
+	arr[j] = temp
 
-def swap(arr, i: int, j: int):
-    temp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = temp
+def main():
+	#arr = [10, 7, 3, 12, 0, 69, 11, 2, 6]
+	arr = [45, 12, 99, 69, 1, 13]
+	QuickSort(arr)
+	print(arr)
+
+if __name__ == '__main__':
+    main()
