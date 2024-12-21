61a29ef7e7e0c47669b9e936328f036cd3f3e38f
Harsh Shastri <hshastri@pendantautomation.com>
2024-12-21 00:28:22 -0500

    Binary search rename

--- DataStructures/BinarySearch.py
+++ DataStructures/BinarySearch.py
@@ -1,20 +1,20 @@
-class BinarySearch:
+class BinarySearchNode:
 
     def __init__(self, data):
         self.root = data
-        self.left: BinarySearch = None
-        self.right: BinarySearch = None
+        self.left: BinarySearchNode = None
+        self.right: BinarySearchNode = None
 
     def insert(self, data):
-        binarySearchNode: BinarySearch = BinarySearch(data)
+        BinarySearchNodeNode: BinarySearchNode = BinarySearchNode(data)
         if self.root > data:
             if self.left == None:
-                self.left = binarySearchNode
+                self.left = BinarySearchNodeNode
             else:
                 self.left.insert(data)
         elif self.root < data:
             if self.right == None:
-                self.right = binarySearchNode
+                self.right = BinarySearchNodeNode
             else:
                 self.right.insert(data)
 
