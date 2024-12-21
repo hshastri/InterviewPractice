f613257d97764b32c99806920027ed42a7b4117c
Harsh Shastri <hshastri@pendantautomation.com>
2024-12-21 02:48:20 -0500

    BST update

--- DataStructures/BinarySearch.py
+++ DataStructures/BinarySearch.py
@@ -1,43 +1,62 @@
-class BinarySearchNode:
+class TreeNode:
+    def __init__(self, val: int):
+        self.val = val
+        self.left: TreeNode = None
+        self.right: TreeNode = None
 
-    def __init__(self, data):
-        self.root = data
-        self.left: BinarySearchNode = None
-        self.right: BinarySearchNode = None
+class BinarySearchTree:
+    def __init__(self):
+        self.root: TreeNode = None
 
-    def insert(self, data):
-        BinarySearchNodeNode: BinarySearchNode = BinarySearchNode(data)
-        if self.root > data:
-            if self.left == None:
-                self.left = BinarySearchNodeNode
-            else:
-                self.left.insert(data)
-        elif self.root < data:
-            if self.right == None:
-                self.right = BinarySearchNodeNode
-            else:
-                self.right.insert(data)
+    def insert(self, val: int):
+        self.root = self._insert_into_bst(self.root, val)
 
-    def find(self, data):
-        if self.root == data:
+    def _insert_into_bst(self, root: TreeNode, val: int) -> TreeNode:
+        if root is None:
+            return TreeNode(val)
+
+        if val < root.val:
+            root.left = self._insert_into_bst(root.left, val)
+        else:
+            root.right = self._insert_into_bst(root.right, val)
+
+        return root
+
+    def find(self, val: int) -> bool:
+        return self._find_rec(self.root, val)
+
+    def _find_rec(self, current: TreeNode, val: int) -> bool:
+        if current is None:
+            return False
+        if current.val == val:
             return True
+        elif val < current.val:
+            return self._find_rec(current.left, val)
+        else:
+            return self._find_rec(current.right, val)
 
-        if self.root > data:
-            if self.left == None:
-                return False
-            else:
-                return self.left.find(data)
-        elif self.root < data:
-            if self.right == None:
-                return False
-            else:
-                return self.right.find(data)
+    def print_in_order(self):
+        self._print_in_order_rec(self.root)
 
-    def printInOrder(self):
-        if self.left != None:
-            self.left.printInOrder()
-        print(self.root)
-        if self.right != None:
-            self.right.printInOrder()
+    def _print_in_order_rec(self, current: TreeNode):
+        if current is not None:
+            self._print_in_order_rec(current.left)
+            print(current.val)
+            self._print_in_order_rec(current.right)
 
+def main():
+    bst = BinarySearchTree()
+    bst.insert(10)
+    bst.insert(5)
+    bst.insert(15)
+    bst.insert(7)
 
+    print("In-order traversal:")
+    bst.print_in_order()
+
+    print("\nFind results:")
+    print(f"Find 7: {bst.find(7)}")  # Should return True
+    print(f"Find 12: {bst.find(12)}")  # Should return False
+
+if __name__ == '__main__':
+    main()
